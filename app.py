ecddaad1fbe7eb831a7d9513c32d6d908690d752
Artyom Iudin <90318913+Tomas542@users.noreply.github.com>
2025-05-04 20:31:25 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,95 @@
+import cv2
+import streamlit as st
+from ultralytics import YOLO
+
+import json
+
+def app():
+    st.header('Object Detection Web App')
+    st.subheader('Artyom Iudin TV set detection web app')
+    model = YOLO('yolov8x-oiv7.pt')
+    data = []
+
+    with st.form("my_form"):
+        uploaded_file = st.file_uploader("Upload video", type=['mp4'])
+        min_confidence = st.slider('Confidence score', 0.2, 1.0)
+        st.form_submit_button(label='Submit')
+    
+    if uploaded_file is not None: 
+        captures = 0
+        times = 0
+        max_tv = 0
+        input_path = uploaded_file.name
+        file_binary = uploaded_file.read()
+
+        with open(input_path, "wb") as temp_file:
+            temp_file.write(file_binary)
+
+        video_stream = cv2.VideoCapture(input_path)
+        
+        width = int(video_stream.get(cv2.CAP_PROP_FRAME_WIDTH)) 
+        height = int(video_stream.get(cv2.CAP_PROP_FRAME_HEIGHT))  
+        fps = int(video_stream.get(cv2.CAP_PROP_FPS)) 
+
+        output_path = input_path.split('.')[0] + '_output.mp4' 
+        out_video = cv2.VideoWriter(output_path, int(cv2.VideoWriter_fourcc(*'mp4v')) , fps, (width, height)) 
+
+        with st.spinner('Processing video...'): 
+            while True:
+                ret, frame = video_stream.read()
+                if not ret:
+                    break
+                result = model(frame)
+                found = False
+                current_tv = 0
+                
+                for detection in result[0].boxes.data:
+                    x0, y0 = (int(detection[0]), int(detection[1]))
+                    x1, y1 = (int(detection[2]), int(detection[3]))
+
+                    score = round(float(detection[4]), 2)
+                    cls = int(detection[5])
+
+                    if model.names[cls] == "Television" and score > min_confidence:
+                        captures += 1
+                        found = True
+                        current_tv += 1
+                        cv2.rectangle(frame, (x0, y0), (x1, y1), (255, 0, 0), 2)
+                        cv2.putText(frame, f'{score}', (x0, y0 - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+
+                if found:
+                    times += 1
+
+                max_tv = max(max_tv, current_tv)
+                detections = result[0].verbose()
+                cv2.putText(frame, detections, (10, 10),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                out_video.write(frame) 
+
+            data.append({
+                "name":input_path,
+                "width": width,
+                "height": height,
+                "fps":fps,
+                "max number of TVs in a frame":max_tv,
+                "avg TVs": captures/times,
+            })
+            with open("data.json", "w") as file:
+                json.dump(data, file)
+
+            video_stream.release()
+            out_video.release()
+            cv2.destroyAllWindows()
+
+        st.video(output_path)
+        st.write(f"Avg TVs: {captures/times}")
+        with open("data.json", "r") as file:
+            st.download_button(
+                label="Download JSON stats",
+                data=file,
+                file_name="data.json"
+            )
+
+if __name__ == "__main__":
+    app()
